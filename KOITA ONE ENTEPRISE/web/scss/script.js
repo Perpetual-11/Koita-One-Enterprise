25d2604f0e9db1f7cb9009246d7c157ea6ad7b32
Perpetual Coder <101705887+Perpetual-11@users.noreply.github.com>
2023-01-17 01:04:39 +0000

    Add files via upload

---
+++ KOITA ONE ENTEPRISE/web/scss/script.js
@@ -0,0 +1,11 @@
+document.getElementById('btnScrollTop')
+    .addEventListener('click', function(){
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        });
+    }
+);
+
+alert('Hello, and Welcome to Koita One Enterprise');
